1aa5d3d65d7962ca6cfce546cf3322c08ed4cfb6
hashaaan <shalithavidanapathirana@gmail.com>
2022-01-09 08:53:31 +0530

    Add new webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env", "@babel/preset-react"] },
       },
     ],
   },
